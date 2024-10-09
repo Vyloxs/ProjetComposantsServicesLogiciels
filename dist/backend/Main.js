bdd0bde2d8977e52edec2ccfa641620b027b3fe2
PaulGoubardLangers <paulo8.pgl@gmail.com>
2024-10-09 12:24:30 +0200

    readable is locked

--- dist/backend/Main.js
+++ dist/backend/Main.js
@@ -1,7 +1,12 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const DMiNer_1 = require("./DMiNer");
+const papaparse_1 = __importDefault(require("papaparse")); // CSV parser: https://www.papaparse.com
 (function Main() {
+    const produits = new Array;
     console.clear();
     //console.info("Working directory: " + __dirname + "\n");
     //console.info("Executable file: " + __filename + "\n");
@@ -9,15 +14,24 @@ const DMiNer_1 = require("./DMiNer");
     const decompression_stream = new DecompressionStream("gzip");
     fetch("https://static.openfoodfacts.org/data/en.openfoodfacts.org.products.csv.gz").then(async (response) => {
         // 'response.body' is a 'ReadableStream' object:
+        //const transformation = new TransformStream(new Compute)
         console.assert(response.body.constructor.name === 'ReadableStream');
         console.assert(response.body.locked === false /*&& response.body.state === 'readable'*/);
-        // console.log(response.body)
-        // console.log(response.text())
         const data_stream = response.body.pipeThrough(decompression_stream);
-        //console.log(data_stream.locked);
         for await (const chunk of Chunks(data_stream.getReader())) {
-            console.log(chunk);
-            break;
+            const { data } = papaparse_1.default.parse((new TextDecoder()).decode(chunk), {
+                header: true,
+                dynamicTyping: true,
+            });
+            for (const key in data) {
+                //console.log(data[key])
+                console.log(data[key].code);
+                console.log(data[key].countries);
+                //produits.push(new Produit2(data[key].code, data[key].countries));
+            }
+            //produits.push(newTodo);
+            //console.info(produits)
+            data_stream.getReader().releaseLock();
         }
     });
     /** CSV */
@@ -30,6 +44,12 @@ const DMiNer_1 = require("./DMiNer");
     // OpenSLR_org_88.Data().then(_ => console.info("'Data' done..."));
     //OpenSLR_org_88.Test();
 })();
+class Produit2 {
+    constructor(code, countries) {
+        this.code = code;
+        this.countries = countries;
+    }
+}
 function Chunks(stream_reader) {
     return {
         async *[Symbol.asyncIterator]() {
--- ts/backend/Main.ts
+++ ts/backend/Main.ts
@@ -2,10 +2,13 @@ import {DMiNer} from "./DMiNer";
 import {version} from "@tensorflow/tfjs-node";
 import OpenSLR_org_88 from "./OpenSLR_org_88/OpenSLR_org_88";
 import path from "node:path";
+import { Writable } from "node:stream";
+import { barchart } from "@tensorflow/tfjs-vis/dist/render/barchart";
+import Papa from "papaparse"; // CSV parser: https://www.papaparse.com
+
 
 (function Main() {
-
-    
+    const produits: Array<Produit> = new Array<Produit>;
     console.clear();
     //console.info("Working directory: " + __dirname + "\n");
     //console.info("Executable file: " + __filename + "\n");
@@ -14,18 +17,37 @@ import path from "node:path";
     const decompression_stream = new DecompressionStream("gzip");
 fetch("https://static.openfoodfacts.org/data/en.openfoodfacts.org.products.csv.gz").then(async (response) => {
     // 'response.body' is a 'ReadableStream' object:
+    //const transformation = new TransformStream(new Compute)
     console.assert(response.body.constructor.name === 'ReadableStream');
     console.assert(response.body.locked === false /*&& response.body.state === 'readable'*/);
-   // console.log(response.body)
-   // console.log(response.text())
-    const data_stream: ReadableStream<number> = response.body.pipeThrough(decompression_stream);
-    //console.log(data_stream.locked);
+    const data_stream: ReadableStream<Array<number>> = response.body.pipeThrough(decompression_stream);
     
+
     for await (const chunk of Chunks(data_stream.getReader())) {
-        console.log(chunk);
-        break;
+        const { data } = Papa.parse((new TextDecoder()).decode(chunk), {
+            header: true,
+            dynamicTyping: true,
+        })
+
+       
+        for(const key in data){
+            //console.log(data[key])
+            console.log(data[key].code);
+            console.log(data[key].countries);
+
+            //produits.push(new Produit2(data[key].code, data[key].countries));
+        }
+        //produits.push(newTodo);
+        //console.info(produits)
+        data_stream.getReader().releaseLock();
+
+        
     }
+
+
+    
 });
+
 
 
 
@@ -37,6 +59,7 @@ fetch("https://static.openfoodfacts.org/data/en.openfoodfacts.org.products.csv.g
     /** End of CSV */
 
     DMiNer.Get_DMN().then(() => {
+        
         // DMiNer.Test_TensorFlow_js_API();
     }); // Default example...
 
@@ -44,6 +67,31 @@ fetch("https://static.openfoodfacts.org/data/en.openfoodfacts.org.products.csv.g
 
     //OpenSLR_org_88.Test();
 })();
+
+class Produit2 {
+    code:number
+    countries:string
+
+    constructor(code, countries){
+        this.code = code;
+        this.countries = countries;
+    }
+
+
+
+}
+
+
+interface Produit {
+    "code":number,
+    "countries":string
+
+
+
+}
+
+
+
 
 
 function Chunks(stream_reader: ReadableStreamDefaultReader) {
